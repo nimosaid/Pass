7b9356f42e62a436acbfca8b0c3443fb1385b5b8
nimosaid <saidnimo123@gmail.com>
2019-07-19 10:45:28 +0300

    add changes

--- password_locker.py
+++ password_locker.py
@@ -1,1 +1,13 @@
 # ! let's get started ! #
+import pyperclip
+from user_info User, import User, Info
+
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+
+	new_user = User(fname,lname,password)
+	return new_user
+
+	
